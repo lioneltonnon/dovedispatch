c5e859f7500085f2b1cab2fd644b90d412a851e7
lioneltonnon <tonnonlionel@gmail.com>
2024-08-06 19:45:44 +0300

    Add SenderExecutor to implement sender logic and integrate with SenderQueueService

---
+++ src/main/java/com/lioneltonnon/dovedispatch/senders/SenderExecutor.java
@@ -0,0 +1,27 @@
+package com.lioneltonnon.dovedispatch.senders;
+
+import com.lioneltonnon.dovedispatch.role.RoleExecutor;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Component
+public class SenderExecutor implements RoleExecutor, ISender {
+
+    private final SenderQueueService senderQueueService;
+
+    @Autowired
+    public SenderExecutor(SenderQueueService senderQueueService) {
+        this.senderQueueService = senderQueueService;
+    }
+
+    @Override
+    public void execute() {
+        System.out.println("Starting producer...");
+        send("Hello from producer");
+    }
+
+    @Override
+    public void send(String message) {
+        senderQueueService.sendMessage(message);
+    }
+}
