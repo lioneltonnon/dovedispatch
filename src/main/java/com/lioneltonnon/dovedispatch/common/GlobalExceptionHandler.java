5d6d18c588faaf8365eb8e5a9b877341441c4348
lioneltonnon <tonnonlionel@gmail.com>
2024-08-06 20:01:36 +0300

    Refactor shared package to 'common'

--- src/main/java/com/lioneltonnon/dovedispatch/common/GlobalExceptionHandler.java
+++ src/main/java/com/lioneltonnon/dovedispatch/common/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.lioneltonnon.dovedispatch.shared;
+package com.lioneltonnon.dovedispatch.common;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
--- src/main/java/com/lioneltonnon/dovedispatch/common/QueueOperations.java
+++ src/main/java/com/lioneltonnon/dovedispatch/common/QueueOperations.java
@@ -1,4 +1,4 @@
-package com.lioneltonnon.dovedispatch.shared;
+package com.lioneltonnon.dovedispatch.common;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
