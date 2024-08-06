60af1512eadd21a0c12a4a550f6bbda19ec22c27
lioneltonnon <tonnonlionel@gmail.com>
2024-08-06 17:17:06 +0300

    Initial commit with SpringInitializr boilerplate

---
+++ src/main/java/com/lioneltonnon/dovedispatch/DovedispatchApplication.java
@@ -0,0 +1,13 @@
+package com.lioneltonnon.dovedispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DovedispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DovedispatchApplication.class, args);
+	}
+
+}
