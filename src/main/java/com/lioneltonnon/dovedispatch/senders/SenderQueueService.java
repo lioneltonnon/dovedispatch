240786da650ffc5fbd45ea84e9c59d14d53cd2e7
lioneltonnon <tonnonlionel@gmail.com>
2024-08-06 21:27:30 +0300

    Refactor SenderQueueService to implement ISender interface

--- src/main/java/com/lioneltonnon/dovedispatch/senders/SenderQueueService.java
+++ src/main/java/com/lioneltonnon/dovedispatch/senders/SenderQueueService.java
@@ -1,24 +1,22 @@
 package com.lioneltonnon.dovedispatch.senders;
 
-import com.lioneltonnon.dovedispatch.config.ApplicationProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SenderQueueService {
+public class SenderQueueService implements ISender {
 
     private final RabbitTemplate rabbitTemplate;
-    private final ApplicationProperties properties;
 
     @Autowired
-    public SenderQueueService(RabbitTemplate rabbitTemplate, ApplicationProperties properties) {
+    public SenderQueueService(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
-        this.properties = properties;
     }
 
-    public void sendMessage(String message) {
-        rabbitTemplate.convertAndSend(properties.getRabbitmq().getQueue(), message);
+    @Override
+    public void send(String message) {
+        rabbitTemplate.convertAndSend("defaultQueue", message);
         System.out.println("Sent message: " + message);
     }
 }
