49d389b24c51a448d8d2bd3c5e90b14ba84c5f3d
lioneltonnon <tonnonlionel@gmail.com>
2024-08-07 11:23:43 +0300

    Add exchange and routing key to SenderQueueService

--- src/main/java/com/lioneltonnon/dovedispatch/senders/SenderQueueService.java
+++ src/main/java/com/lioneltonnon/dovedispatch/senders/SenderQueueService.java
@@ -1,5 +1,6 @@
 package com.lioneltonnon.dovedispatch.senders;
 
+import com.lioneltonnon.dovedispatch.config.ApplicationProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,15 +9,19 @@ import org.springframework.stereotype.Service;
 public class SenderQueueService implements ISender {
 
     private final RabbitTemplate rabbitTemplate;
+    private final String exchangeName;
+    private final String routingKey;
 
     @Autowired
-    public SenderQueueService(RabbitTemplate rabbitTemplate) {
+    public SenderQueueService(RabbitTemplate rabbitTemplate, ApplicationProperties customProperties) {
         this.rabbitTemplate = rabbitTemplate;
+        this.exchangeName = customProperties.getExchange();
+        this.routingKey = customProperties.getRoutingKey();
     }
 
     @Override
     public void send(String message) {
-        rabbitTemplate.convertAndSend("defaultQueue", message);
+        rabbitTemplate.convertAndSend(exchangeName, routingKey, message);
         System.out.println("Sent message: " + message);
     }
 }
